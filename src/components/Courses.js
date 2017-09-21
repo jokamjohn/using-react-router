b4d353cea7a6c18ff1d70fb8b14592f2fe73de58
John Kagga <johnkagga@gmail.com>
2017-09-21 14:04:12 +0300

    Using redirects and also managing active state for main and sub routes

--- src/components/Courses.js
+++ src/components/Courses.js
@@ -1,18 +1,27 @@
 import React from 'react';
+import {NavLink, Redirect, Route} from 'react-router-dom';
+import HTML from "./courses/HTML";
+import CSS from "./courses/CSS";
+import JavaScript from "./courses/JavaScript";
 
 const Courses = () => (
-  <div className="main-content courses">
-    <div className="course-header group">
-      <h2>Courses</h2> 
-      <ul className="course-nav">
-        <li><a href='/courses/html'>HTML</a></li>
-        <li><a href='/courses/css'>CSS</a></li>
-        <li><a href='/courses/javascript'>JavaScript</a></li>
-      </ul>
+    <div className="main-content courses">
+        <div className="course-header group">
+            <h2>Courses</h2>
+            <ul className="course-nav">
+                <li><NavLink to='/courses/html'>HTML</NavLink></li>
+                <li><NavLink to='/courses/css'>CSS</NavLink></li>
+                <li><NavLink to='/courses/javascript'>JavaScript</NavLink></li>
+            </ul>
+        </div>
+
+        {/* Write routes here... */}
+        <Route exact path="/courses" component={() => <Redirect to="/courses/html"/>}/>
+        <Route path="/courses/html" component={HTML}/>
+        <Route path="/courses/css" component={CSS}/>
+        <Route path="/courses/javascript" component={JavaScript}/>
+
     </div>
-    
-    {/* Write routes here... */}
-  </div>
 );
 
 export default Courses;
