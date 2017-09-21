6d03a34ff547f9601d8e0db3a4d89f9db2fd9db7
John Kagga <johnkagga@gmail.com>
2017-09-21 14:52:40 +0300

    Using URL parameters

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,7 @@ import Header from "./Header";
 import Teachers from "./Teachers";
 import Courses from "./Courses";
 import NotFound from "./NotFound";
+import Featured from "./Featured";
 
 const App = () => (
     <BrowserRouter>
@@ -14,7 +15,8 @@ const App = () => (
             <Switch>
                 <Route exact path="/" component={Home}/>
                 <Route path="/about" render={() => <About title="About"/>}/>
-                <Route path="/teachers" component={Teachers}/>
+                <Route exact path="/teachers" component={Teachers}/>
+                <Route path="/teachers/:skill/:name" component={Featured}/>
                 <Route path="/courses" component={Courses}/>
                 <Route component={NotFound}/>
             </Switch>
